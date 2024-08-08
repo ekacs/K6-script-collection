9ae7ef2a3ad96b901777c44b5c63889b05fe511c
ekacs <ekacs@yandex.com>
2024-08-08 09:35:49 +0700

    edit

---
+++ sipencatar/skenario7.2.js
@@ -0,0 +1,38 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+import encoding from 'k6/encoding';
+
+export let options = {
+    stages: [
+        { duration: '30m', target: 50 },
+        { duration: '50m', target: 100 },
+        { duration: '30m', target: 50 },
+        { duration: '50m', target: 100 },
+        { duration: '2m', target: 0 },
+    ],
+    thresholds: {
+        http_req_duration: ['p(95)<2000'], // 95% dari permintaan harus kurang dari 2 detik
+    },
+};
+
+export default function () {
+    let credentials = 'adminpusat@sipencatar.app:KemenhubAPusat';
+    let encodedCredentials = `Basic ${encoding.b64encode(credentials)}`;
+
+    let params = {
+        headers: {
+            'Authorization': encodedCredentials,
+        },
+    };
+
+    let res = http.get('https://api-sipencatar.dephub.go.id/v1/psikotes', params);
+    
+    // Log waktu respons
+    console.log(`Response time was ${res.timings.duration} ms`);
+    
+    check(res, {
+        'status was 200': (r) => r.status === 200,
+        'response time under 2s': (r) => r.timings.duration < 2000, // Memeriksa waktu respons
+    });
+    sleep(1);
+}
